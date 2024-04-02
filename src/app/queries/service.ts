91a70d01df341859e8041a1fae6d04d5650665af
alighadyani <ali.ghadyani2001@gmail.com>
2024-04-02 21:48:59 +0330

    Adding Add Query, Custom Hook and Types

--- src/app/queries/service.ts
+++ src/app/queries/service.ts
@@ -5,6 +5,8 @@ import { HttpClient } from "./http-client";
 const Service = {
   contact: {
     get: () => HttpClient.get<any>(API_ENDPOINTS.CONTACTS),
+    add: (params: Contact) =>
+      HttpClient.post<Contact>(API_ENDPOINTS.CONTACTS, { ...params }),
   },
 };
 export default Service;
