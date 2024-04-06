6338d2f0eb60f0262eb088e8a849523dfc6d8bde
alighadyani <ali.ghadyani2001@gmail.com>
2024-04-06 21:11:41 +0330

    Adding Toasts

--- src/app/contact/add/page.tsx
+++ src/app/contact/add/page.tsx
@@ -2,6 +2,8 @@
 import { Contact } from "@/app/models/contacts";
 import { useAddContacts } from "@/app/queries/contacts";
 import ContactForm from "../ContactForm";
+import toast from "react-hot-toast";
+import { Metadata } from "next";
 
 // export const metadata: Metadata = {
 //   title: "ایجاد",
@@ -12,10 +14,26 @@ const Add = () => {
   const onSubmit = async (contact: Contact) => {
     await addContact(contact)
       .then((order) => {
-        console.log("successful");
+        toast.success("مخاطب با موفقیت ایجاد شد.", {
+          icon: "🚀",
+          style: {
+            borderRadius: "4px",
+            border: "1px solid #50FA7B",
+            background: "#282A36",
+            color: "#fff",
+          },
+        });
       })
       .catch((_errors) => {
-        console.log("failed");
+        toast.error("در ایجاد مخاطب مشکلی پیش آمده است.", {
+          icon: "🤔",
+          style: {
+            borderRadius: "4px",
+            border: "1px solid #FF5555",
+            background: "#282A36",
+            color: "#fff",
+          },
+        });
       });
   };
 
