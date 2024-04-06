0d57cb2b452bd68ae77d6212fe5d4e43cf9ecdc5
AliRzGho <googlialireza@gmail.com>
2024-04-05 17:55:31 -0700

    Adding Detail Page and Detail Query

---
+++ src/app/contact/[contactID]/detail/DetailPage.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useDetailContacts } from "@/app/queries/contacts";
+import ContactForm from "../../ContactForm";
+
+const DetailPage = ({ id }: { id: number }) => {
+  const { data: contact, isPending } = useDetailContacts(id);
+  return contact && <ContactForm formMode="detail" defaultValue={contact} />;
+};
+export default DetailPage;
