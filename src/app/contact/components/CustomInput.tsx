a39fc6aaa0f95c407e913d7d7af0f3c22ff45e07
Morteza <117587177+Morteza-hp@users.noreply.github.com>
2025-02-10 20:35:33 +0330

    Updated TailwindCSS

--- src/app/contact/components/CustomInput.tsx
+++ src/app/contact/components/CustomInput.tsx
@@ -19,10 +19,10 @@ const CustomInput = ({ icon, text, type, name }: CustomInputProps): JSX.Element 
           type={type}
           {...register(name)}
           id={name}
-          className="peer p-4 ps-11 block w-full rounded-lg text-sm placeholder:text-transparent transition ease-in-out duration-200 hover:border-Purple focus:ring-Purple focus:border-Purple bg-CurrentLine text-white focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2 focus:bg-Background"
+          className="peer p-4 ps-11 block w-full rounded-lg text-sm placeholder:text-transparent transition ease-in-out duration-200 hover:border-Purple focus:ring-Purple focus:border-Purple bg-CurrentLine text-white focus:pt-6 focus:pb-2 not-placeholder-shown:pt-6 not-placeholder-shown:pb-2 autofill:pt-6 autofill:pb-2 focus:bg-Background"
           placeholder="you@email.com"
         />
-        <label className="absolute px-4 ps-11 top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 text-gray-400 peer-focus:text-xs peer-focus:-translate-y-2 peer-focus:text-Purple peer-[:not(:placeholder-shown)]:text-xs peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:text-Purple">
+        <label className="absolute px-4 ps-11 top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 text-gray-400 peer-focus:text-xs peer-focus:-translate-y-2 peer-focus:text-Purple peer-not-placeholder-shown:text-xs peer-not-placeholder-shown:-translate-y-2 peer-not-placeholder-shown:text-Purple">
           {text}
         </label>
         <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4">
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
       <h1 className="text-9xl font-extrabold text-white tracking-widest">
         404
       </h1>
-      <div className="bg-Purple px-2 text-sm rounded rotate-12 absolute">
+      <div className="bg-Purple px-2 text-sm rounded-sm rotate-12 absolute">
         صفحه ایی که دنبالش هستی رو پیدا نکردیم 🤔
       </div>
       <button className="mt-5">
